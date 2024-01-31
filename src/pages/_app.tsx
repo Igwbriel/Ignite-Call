1b14c332f0dbd7bf33f0f6c6d3efdb7fde0865e1
Gabriel Dias <GabrielDias7820@gmail.com>
2024-01-30 23:29:02 -0300

    {update} configuração do ESlint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import type { AppProps } from "next/app";
-import { globalStyles } from "../styles/global";
+import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import { Heading, Text } from '@ignite-ui/react'
 
 export default function Home() {
-  return (
-    <Heading as="h1">Hello World</Heading>
-  );
+  return <Heading as="h1">Hello World</Heading>
 }
