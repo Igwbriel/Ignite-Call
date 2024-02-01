66b988e68b02e21ab729c16042675a90a2885a15
Gabriel Dias <GabrielDias7820@gmail.com>
2024-01-31 23:39:57 -0300

    {update} - Estrutura visual da Home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,28 @@
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+import PreviewImage from '../../assets/App-preview.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={PreviewImage}
+          height={400}
+          quality={100}
+          priority
+          alt={'Calendário simbolizando a aplicação em funcionamento'}
+        />
+      </Preview>
+    </Container>
+  )
+}
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,1 @@
-import { Heading, Text } from '@ignite-ui/react'
-
-export default function Home() {
-  return <Heading as="h1">Hello World</Heading>
-}
+export { default } from './home'
