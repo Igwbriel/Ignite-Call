7cb54cf2ffa0b5312363fbb84e2d414740732745
Gabriel Dias <GabrielDias7820@gmail.com>
2024-02-20 20:02:20 -0300

    {update} - página de disponibilidade

---
+++ src/pages/register/timer-intervals/index.page.tsx
@@ -0,0 +1,84 @@
+import {
+  Button,
+  Checkbox,
+  Heading,
+  MultiStep,
+  Text,
+  TextInput,
+} from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+import {
+  IntervalBox,
+  IntervalContainer,
+  IntervalDay,
+  IntervalInputs,
+  IntervalItem,
+} from './styles'
+import { ArrowRight } from 'phosphor-react'
+
+export default function TimeIntervals() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Quase lá</Heading>
+        <Text>
+          Defina o intervalo de horários que você está disponível em cada dia da
+          semana.
+        </Text>
+
+        <MultiStep size={4} currentStep={3} />
+      </Header>
+
+      <IntervalBox as="form">
+        <IntervalContainer>
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Segunda-feira</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput
+                size="sm"
+                type="time"
+                step="60"
+                crossOrigin={undefined}
+              ></TextInput>
+              <TextInput
+                size="sm"
+                type="time"
+                step="60"
+                crossOrigin={undefined}
+              ></TextInput>
+            </IntervalInputs>
+          </IntervalItem>
+
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Terça-feira</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput
+                size="sm"
+                type="time"
+                step="60"
+                crossOrigin={undefined}
+              ></TextInput>
+              <TextInput
+                size="sm"
+                type="time"
+                step="60"
+                crossOrigin={undefined}
+              ></TextInput>
+            </IntervalInputs>
+          </IntervalItem>
+        </IntervalContainer>
+
+        <Button>
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </IntervalBox>
+    </Container>
+  )
+}
