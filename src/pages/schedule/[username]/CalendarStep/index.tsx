6a06f5063099cc3009c2fb0d89bddb839af2c476
Gabriel Dias <GabrielDias7820@gmail.com>
2024-02-26 19:42:06 -0300

    {update} - estrutura visual da lista de horários

--- src/pages/schedule/[username]/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/CalendarStep/index.tsx
@@ -1,10 +1,40 @@
 import { Calendar } from '../../../../components/calendar'
-import { Container } from './styles'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            Segunda-feira <span>26 de fevereiro</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem disabled>09:00h</TimePickerItem>
+            <TimePickerItem>10:00h</TimePickerItem>
+            <TimePickerItem>11:00h</TimePickerItem>
+            <TimePickerItem>12:00h</TimePickerItem>
+            <TimePickerItem>13:00h</TimePickerItem>
+            <TimePickerItem>14:00h</TimePickerItem>
+            <TimePickerItem>15:00h</TimePickerItem>
+            <TimePickerItem>16:00h</TimePickerItem>
+            <TimePickerItem>17:00h</TimePickerItem>
+            <TimePickerItem>18:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
