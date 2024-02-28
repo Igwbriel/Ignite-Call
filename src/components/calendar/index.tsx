726be08592c6edc060c33124e97a21d4f33b160d
Gabriel Dias <GabrielDias7820@gmail.com>
2024-02-27 21:52:53 -0300

    {update} - calculando os dias do mês

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -8,7 +8,7 @@ import {
   CalendarTittle,
 } from './styles'
 import { getWeekDays } from '../../utils/get-week-days'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import dayjs from 'dayjs'
 
 export function Calendar() {
@@ -31,8 +31,29 @@ export function Calendar() {
   const shortWeekDays = getWeekDays({ short: true })
 
   const currentMonth = currentDate.format('MMMM')
-
   const currentYear = currentDate.format('YYYY')
+
+  const calendarWeeks = useMemo(() => {
+    const daysInMonthArray = Array.from({
+      length: currentDate.daysInMonth(),
+    }).map((_, i) => {
+      return currentDate.set('date', i + 1)
+    })
+
+    const firstWeekDay = currentDate.get('day')
+
+    const previousMonthFillArray = Array.from({
+      length: firstWeekDay,
+    })
+      .map((_, i) => {
+        return currentDate.subtract(i + 1, 'day')
+      })
+      .reverse()
+
+    return [...previousMonthFillArray, ...daysInMonthArray]
+  }, [currentDate])
+
+  console.log(calendarWeeks)
 
   return (
     <CalendarContainer>
